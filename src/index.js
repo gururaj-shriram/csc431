bfe8e03a0d996f3ff99b198966021c3e83b57296
Gururaj Shriram <gururajshriram@gmail.com>
2018-04-07 15:45:54 -0400

    minor fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // Set port number if it's an argument
-app.set('port', process.enc.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
 	res.send('<h1>Test Response</h1>');
