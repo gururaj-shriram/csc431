729ddf86448d46bf4317f3ba43e8e5d6b4cb6bc0
Gururaj Shriram <gururajshriram@gmail.com>
2018-05-02 21:50:57 -0400

    added stress test for simultaneous downloads. once we remove some sync calls, we should be good :D

--- src/app/tests/download-test.js
+++ src/app/tests/download-test.js
@@ -7,6 +7,7 @@
   also a proof-of-work of the datapackager since a zip file results
 */
 
+const async= require('async');
 const sinon = require('sinon');
 const request = require('request');
 const chai = require('chai');
@@ -15,24 +16,75 @@ const should = chai.should();
 
 const base = 'http://localhost:3000';
 
-describe('end to end download test', () => {
-    it('should take in table parameters and output a zip file', (done) => {
-      const options = {
-        method: 'post',
-        body: { 
-          construccion: '1', 
-          workshop_20170210: '2', 
-          terreno: '3', 
-          formattype: 'csv' 
-        },
-        json: true,
-        url: `${base}/download`
-      };    
+const url = `${base}/download`;
 
-        request(options, (err, res, body) => {
-          res.statusCode.should.equal(200);
-          res.headers['content-type'].should.contain('application/zip');
-          done();
-        });
+const options = {
+    method: 'post',
+    body: { 
+      construccion: '1', 
+      workshop_20170210: '2', 
+      terreno: '3', 
+      formattype: 'csv' 
+    },
+    json: true,
+    url: url
+  };  
+
+function httpPost(url, callback) {
+  request(options,
+    function(err, res, body) {
+      callback(err, body);
+    }
+  );
+}
+
+describe('end to end download test', () => {
+  
+  it('should take in table parameters and output a zip file', (done) => {
+    request(options, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      res.headers['content-type'].should.contain('application/zip');
+      done();
     });
+  });
+  
+  it('should take in 3 simultaneous requests', (done) => {
+    let urls = [];
+
+    for (let i = 0; i < 3; i++) {
+      urls.push(url);
+    }
+
+    async.map(urls, httpPost, function (err, res){
+      if (err) return console.log(err);
+      done();
+    });
+  });
+
+  it('should take in 10 simultaneous requests', (done) => {
+    let urls = [];
+
+    for (let i = 0; i < 10; i++) {
+      urls.push(url);
+    }
+
+    async.map(urls, httpPost, function (err, res){
+      if (err) return console.log(err);
+      done();
+    });
+  });
+
+  it('should take in 100 simultaneous requests', function(done) {
+    this.timeout(2000);
+    let urls = [];
+
+    for (let i = 0; i < 100; i++) {
+      urls.push(url);
+    }
+
+    async.map(urls, httpPost, function (err, res){
+      if (err) return console.log(err);
+      done();
+    });
+  });
 });
